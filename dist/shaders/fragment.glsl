47fc3e25f8d7ea405b7aed0bdca3ddfa72c635dd
cecilia-wsk <cecilia.wielonsky@gmail.com>
2023-08-26 10:38:58 +0200

    dist folder

--- dist/shaders/fragment.glsl
+++
@@ -1,9 +0,0 @@
-varying vec3 vUv;
-uniform float uTime;
-uniform sampler2D uTexture;
-
-void main() {
-    float time = uTime;
-    vec4 color = texture2D(uTexture,vUv);
-    gl_FragColor = color;
-}
