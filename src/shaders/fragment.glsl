433e35f1bfaae8355025fae6176cf3d6b5e078dc
cecilia-wsk <cecilia.wielonsky@gmail.com>
2023-08-26 10:31:53 +0200

    restore text texture

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -6,10 +6,10 @@ varying vec2 vUv;
 void main() {
     float time = uTime;
     vec2 uv = vUv;
-    vec2 repeat = vec2(6.0, 7.0);
+    vec2 repeat = vec2(4.0, 6.0);
     uv.x += sin(uv.y - time) * 0.05; 
-    uv = fract( uv * repeat - vec2(0.0, time));
+    uv = fract( uv * repeat - vec2(0.5, time));
     vec4 color = texture2D(uTexture, uv);
-    // gl_FragColor = color;
-     gl_FragColor = vec4( vec3(uv, 0.75), 0.8 );
+    gl_FragColor = color;
+    // gl_FragColor = vec4( vec3(uv, 0.75), 0.8 );
 }
